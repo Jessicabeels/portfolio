185f465605dbb366268d11a5592ee458adc5e3f9
Jessica Beeli <jmbeeli@gmail.com>
2019-04-17 09:07:06 -0700

    styling

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,17 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            website by Jessica Beeli 2019. Made with React. 
+        <div className="footer-container">
+            Portfolio by Jessica Beeli. Made with React. 
+            <a className="linkedin-footer" href='https://www.linkedin.com/in/jessicabeeli/'>
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a className="github-footer" href='https://github.com/Jessicabeels'>
+                <i class="fab fa-github"></i>
+            </a>
+            <a className="twitter-footer" href='https://twitter.com/jessicabeeli'>
+                <i class="fab fa-twitter"></i>
+            </a>
         </div>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,9 +10,10 @@ const Projects = () => {
                     <div class="boxA ">
                         <div className="transbox">
                         <h4>Space Dinos Adventures</h4>
-                        <p>Protect Earth from falling meteorites as Donny the Dino in this fullstack application. </p>
-                        <p>Learned user authentication</p>
-                        <p> My role: </p>
+                        <h6>Play this game with the objective of protecting Earth from falling astroids as Donny the Dino.</h6>
+                        <p>Fullstack application built with React, implemented user authentication, created backend to store users, high scores.</p>
+                        <p>Graphics built using Adobe Photoshop.</p>
+                        <p> </p>
                         <a className="project-link" href="todolistbyjess.surge.sh">Visit Site</a>
                         </div>      
                     </div>
@@ -22,16 +23,19 @@ const Projects = () => {
                     <div class="boxB ">
                         <div className="transbox">
                             <h4>Shelter Safe Haven </h4>
-                            <p>This website is disguised as a gardening app upon arrival, but when entering
-                                the site, you'll find resources for survivors in abusive relationships. We created a
-                                database that consolidates shelters from 5 states, and hope to add more in the future.  </p>
+                            <h6>This website is disguised as a gardening app upon arrival, but when entering
+                                the site, you'll find resources for survivors of sexual violence and abusive relationships. We created a
+                                database that consolidates shelters from 5 states, and hope to add more in the future.</h6>
+                            <p>Fullstack application built with React with custom shelter database. Features a chatbot that conducts a danger assessment. </p>
                             <a className="project-link" href="todolistbyjess.surge.sh">Visit Site</a>
                             </div>
                     </div>
                     <div class="boxC ">
                         <div className="transbox">
                             <h4>Pub Pocket</h4> 
-                            <p> Resource for academics to search through journals, and save them. </p>  
+                            <h6> Resource for academics to search through journals, and save them. </h6> 
+                            <p>Frontend application built with React. Gets 2 apis and uses local storage to save articles of interest.</p>
+
                             <a className="project-link" href="todolistbyjess.surge.sh">Visit Site</a> 
                         </div>
                     </div>
@@ -39,6 +43,7 @@ const Projects = () => {
                     <div class="boxD ">
                         <div className="transbox">
                             <h4>Task Habit </h4>
+                            <p>Todo list built in vanilla Javascript with Axios. Fully CRUD application.</p>
                             <a className="project-link" href="todolistbyjess.surge.sh">Visit Site</a>
                         </div>
                     </div>
@@ -46,6 +51,7 @@ const Projects = () => {
                     <div class="boxE ">
                         <div className="transbox">
                             <h4>Hello My Name Is...</h4> 
+                            <p> React application in which you can add and post your basic information.</p>
                             <a className="project-link" href="hellomynamebyjess.surge.sh">Visit Site</a>
                         </div>
                     </div>
@@ -53,7 +59,7 @@ const Projects = () => {
                     <div class="boxF ">
                         <div className="transbox">
                             <h4>Photography Website</h4>
-                            <p>Built with Javascript</p>
+                            <p>Basic website built with vanilla Javascript using forms. </p>
                             <a className="project-link" href="http://photosbyjess.surge.sh/">Visit Site</a>
                         </div>
                     </div>
